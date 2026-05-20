daa9d0a563998d3ea7ea744c2afb7797db5468e4
Aditya Suryawanshi <adityassuryawanshi25@gmail.com>
2026-05-20 22:44:51 +0530

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,15 +1,13 @@
 data = [
     {
-        "instruction": "create exam notes from the pdf",
-        "input": "Chess is a two-player strategy board game played on an 8x8 grid with 64 squares",
-        "output": "Chess : A two-player strategy board game played on an 8x8 grid with 64 squares"
-    }
-]
+        "instruction": "Create exam notes from the PDF",
+        "input": "Chess is a two-player strategy board game played on an 8x8 grid with 64 squares.",
+        "output": "Chess: A two-player strategy board game played on an 8x8 grid with 64 squares."
+    },
 
-data = [
     {
-        "instruction": "Create exam notes on the pdf",
-        "input": "Football is a family of team sports where two teams compete to advance a ball into the opposing team's goal or end zone",
-        "output": "Football : A team sports where two teams compete to advance a ball into the opposing team's goal "
+        "instruction": "Create exam notes from the PDF",
+        "input": "Football is a family of team sports where two teams compete to advance a ball into the opposing team's goal or end zone.",
+        "output": "Football: A team sport where two teams compete to advance a ball into the opposing team's goal."
     }
 ]
